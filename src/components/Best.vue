8d9da003d11b367795939558f0c5dfc7a96cf0d4
rhddbrhdfyd <jjklqwe16@gmail.com>
2025-06-04 16:45:32 +0900

    수정

---
+++ src/components/Best.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BestDesktop from './BestDesktop.vue';
+import BestMobile from './BestMobile.vue';
+import { computed } from 'vue'
+
+const isMobile = computed(() => window.innerWidth <= 768)
+</script>
+
+<template>
+  <BestMobile v-if="isMobile" />
+  <BestDesktop v-else />
+</template>
